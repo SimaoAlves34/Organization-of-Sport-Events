7d46e0ff0bae94ada3f8cf85d9cb917ac6bf6f92
Simão Alves <108157731+SimaoAlves34@users.noreply.github.com>
2023-07-20 16:54:24 +0100

    Add files via upload

---
+++ NaoFederado.java
@@ -0,0 +1,21 @@
+package trab1;
+
+    /**
+    *Criação da classe NaoFederado
+    *Herança da class Atleta
+    */
+
+public class NaoFederado extends Atleta{
+    
+     /**
+    *Atributos da classe NaoFederado
+    */
+    
+    private boolean saude;
+    
+    /** Método Construtor da Classe NaoFederado */
+    
+    public NaoFederado(String nif){
+        super(nif);
+    }
+}
